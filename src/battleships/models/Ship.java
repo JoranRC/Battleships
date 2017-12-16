7de3b002d03beb4fd82b00a473ab34cae990abb1
JoranRC <33453681+JoranRC@users.noreply.github.com>
2017-12-16 13:38:44 +0100

    Added Ship model

--- src/battleships/models/Ship.java
+++ src/battleships/models/Ship.java
@@ -1,4 +1,40 @@
 package battleships.models;
 
+import java.util.ArrayList;
+
 public class Ship {
+
+    private boolean orientation;
+    private int shipLength;
+    private ArrayList<Integer> location;
+
+    public Ship(boolean orientation, int shipLength, ArrayList<Integer> location) {
+        this.orientation = orientation;
+        this.shipLength = shipLength;
+        this.location = location;
+    }
+
+    public boolean isOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(boolean orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getShipLength() {
+        return shipLength;
+    }
+
+    public void setShipLength(int shipLength) {
+        this.shipLength = shipLength;
+    }
+
+    public ArrayList<Integer> getLocation() {
+        return location;
+    }
+
+    public void setLocation(ArrayList<Integer> location) {
+        this.location = location;
+    }
 }
